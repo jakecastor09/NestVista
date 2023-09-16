064cc2c1b9cc6051aa03a0d3f8fa1f80febd86c0
jakecastor09 <rencelaw70@gmail.com>
2023-09-16 21:00:29 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className='text-2xl text-slate-700 text-bold uppercase'>manok</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        pryGreen: '#5B9A8B',
+        pryBlue: '#252B48',
+        lightGray: '#FAFAFA',
+        grayGreen: '#EDF3F2',
+        textTitle: '#333333',
+        textBody: '#666666',
+        infield: '#B8B8B8',
+        disable: '#EBEBEB',
+      },
+    },
+  },
+  plugins: [],
+};
