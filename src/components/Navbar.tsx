8db8ffa46efea84b98ffebaa189c08fa133ff4b8
jakecastor09 <rencelaw70@gmail.com>
2023-09-23 12:26:56 +0800

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const [clickPage, setClickPage] = useState('home');
+  const styleNavItem = 'font-semibold text-textTitle';
   return (
     <nav className='p-4 md:py-4 md:px-6 xl:p-6'>
       <div className='flex justify-between items-center'>
@@ -10,16 +13,36 @@ const Navbar = () => {
             <GiHamburgerMenu size='24px' />
           </div>
           <ul className='hidden space-x-12 lg:flex text-textBody'>
-            <li className='font-semibold text-textTitle'>
+            <li
+              className={`${clickPage === 'home' && styleNavItem}`}
+              onClick={() => setClickPage('home')}
+            >
               <Link to={'/'}> Home</Link>
             </li>
-            <li>
+            <li
+              className={`${clickPage === 'listing' && styleNavItem}`}
+              onClick={() => setClickPage('listing')}
+            >
               <Link to={'/listing'}> Listing</Link>
             </li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>About</li>
-            <li>Contact Us</li>
+            <li
+              className={`${clickPage === 'services' && styleNavItem}`}
+              onClick={() => setClickPage('services')}
+            >
+              <Link to={'/services'}> Services</Link>
+            </li>
+            <li
+              className={`${clickPage === 'about' && styleNavItem}`}
+              onClick={() => setClickPage('about')}
+            >
+              <Link to={'/about'}>About</Link>
+            </li>
+            <li
+              className={`${clickPage === 'contact' && styleNavItem}`}
+              onClick={() => setClickPage('contact')}
+            >
+              <Link to={'/contact'}>Contact Us</Link>
+            </li>
           </ul>
           <button className='px-6 py-2 rounded-full bg-pryGreen text-white font-medium hover:opacity-90 duration-200'>
             Get a Quote
